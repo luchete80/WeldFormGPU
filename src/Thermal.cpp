f5411de2e6c40bbe9bc2cefdb403667b29d9c485
luciano nb <luciano.buglioni@gmail.com>
2022-02-17 18:54:58 -0300

    removed previous dTdt version

--- src/Thermal.cpp
+++ src/Thermal.cpp
@@ -5,49 +5,6 @@ using namespace std;
 
 namespace SPH {
 
-inline void Domain_d::CalcTempInc () {
-	double di=0.0,dj=0.0,mi=0.0,mj=0.0;
-	
-	std::vector < double> temp(Particles.Size());
-	
-	#pragma omp parallel for schedule (static) num_threads(Nproc) //LUCIANO: THIS IS DONE SAME AS PrimaryComputeAcceleration
-	for ( size_t k = 0; k < Nproc ; k++) {
-		Particle *P1,*P2;
-		Vec3_t xij;
-		double h,GK;
-		//TODO: DO THE LOCK PARALLEL THING
-		// Summing the smoothed pressure, velocity and stress for fixed particles from neighbour particles
-		//std::vector <double> temp()=0;
-		//cout << "fixed pair size: "<<FSMPairs[k].Size()<<endl;
-		//cout << "Particles size: " << Particles.Size()<<endl;
-		for (size_t a=0; a<SMPairs[k].Size();a++) {//Same Material Pairs, Similar to Domain::LastComputeAcceleration ()
-			//cout << "a: " << a << "p1: " << SMPairs[k][a].first << ", p2: "<< SMPairs[k][a].second<<endl;
-			P1	= Particles[SMPairs[k][a].first];
-			P2	= Particles[SMPairs[k][a].second];
-			xij	= P1->x - P2->x;
-			h	= (P1->h+P2->h)/2.0;
-			GK	= GradKernel(Dimension, KernelType, norm(xij)/h, h);
-			di = P1->Density; mi = P1->Mass;
-			dj = P2->Density; mj = P2->Mass;
-
-			//Frasier  Eqn 3.99 dTi/dt= 1/(rhoi_CPi) * Sum_j(mj/rho_j * 4*ki kj/ (ki + kj ) (Ti - Tj)  ) 
-			//LUCIANO: TODO EXCLUDE THIS PRODUCT
-			temp [SMPairs[k][a].first] += mj/dj * 4. * ( P1->k_T * P2->k_T) / (P1->k_T + P2->k_T) * ( P1->T - P2->T) * dot( xij , GK*xij );
-			omp_set_lock(&P1->my_lock);
-				P1->dTdt		+= 1./(di*P1->cp_T) * ( temp + P1->q_conv );
-			omp_unset_lock(&P1->my_lock);
-			// Locking the particle 2 for updating the properties
-			omp_set_lock(&P2->my_lock);
-				P2->dTdt		-= 1./(di*P2->cp_T) * ( temp + P2->q_conv );
-			omp_unset_lock(&P2->my_lock);
-				//cout << "temp: "<<dot( xij , GK*xij )<<endl;
-		}
-
-	}//Nproc
-
-}
-
-
 //NON PARALLEL TEST VERSION
 inline void Domain::CalcTempInc () {
 	double di=0.0,dj=0.0,mi=0.0,mj=0.0;
